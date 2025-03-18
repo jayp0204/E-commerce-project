b9e4c5faf0590a66f13be8facfe4c0fbf86e83c1
jayp0204 <161871900+jayp0204@users.noreply.github.com>
2025-03-19 00:39:53 +0530

    authentication as well as toast implementation for registration complete

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import CommonForm from "@/components/common/form";
+import { registerFormControls } from "@/config";
+import { registerUser } from "@/store/auth-slice";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+const initialState = {
+  userName: "",
+  email: "",
+  password: "",
+};
 
 const AuthRegister = () => {
-  return (
-    <div>AuthRegister</div>
-  )
-}
+  const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // const { toast } = useSonner();
 
-export default AuthRegister
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dispatch(registerUser(formData)).then((data) => {
+      if (data?.payload?.success) {
+        toast(data?.payload?.message);
+        navigate("/auth/login");
+      }
+      // console.log(data, "data")
+    });
+  };
+
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Create new account
+        </h1>
+        <p className="mt-2">
+          Already have an account
+          <Link
+            to="/auth/login"
+            className="font-medium ml-2 text-primary hover:underline"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={registerFormControls}
+        buttonText={"Sign Up"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
+};
+
+export default AuthRegister;
